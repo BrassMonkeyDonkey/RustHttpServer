bb761e67998f2328e12a1d226dd0b3416d9e30ae
BrassMonkeyDonkey <peraldi.kevin@gmail.com>
2020-11-08 18:33:56 +0100

    extract query out of the request path

--- src/http/request.rs
+++ src/http/request.rs
@@ -18,13 +18,19 @@ impl TryFrom<&[u8]> for Request {
         let request = str::from_utf8(buf)?;
 
         let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
-        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
         let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
         if protocol != "HTTP/1.1" {
             return Err(ParseError::InvalidProtocol);
         }
 
         let method: Method = method.parse()?;
+
+        let mut query = None;
+        if let Some(i) = path.find('?') {
+            query = Some(&path[i + 1..]);
+            path = &path[..i];
+        }
 
         unimplemented!();
     }
